b928d2bd838d7d44ed847de6a8031a82e4a984b8
dhruv456 <jdhruv456@gmail.com>
2021-09-25 16:36:54 +0530

    bfs

--- BFS.java
+++ BFS.java
@@ -2,71 +2,71 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 
 public class BFS {
-    static class Vertex {
-        int value;
-        String color = "white";
-        int distance = 9999999;
-        Vertex predessorVertex = null;
-    }
+    
     // adding edge function to create graph (Bidirectional)
-    static void addEdge(ArrayList<Vertex> vertexArrayList, ArrayList<ArrayList<Vertex> > adj, int u, int v) {
-        adj.get(u).add(vertexArrayList.get(v));
-        adj.get(v).add(vertexArrayList.get(u));
+    static void addEdge(ArrayList<ArrayList<Integer> > adj, int u, int v) {
+        adj.get(u).add(v);
+        adj.get(v).add(u);
     }
     // printing graph in adjency manner
-    static void printAdj(ArrayList<ArrayList<Vertex> > adj) {
+    static void printAdj(ArrayList<ArrayList<Integer> > adj) {
         for (int i = 0; i < adj.size(); i++) {
             System.out.print(i + "--->");
             for (int j = 0; j < adj.get(i).size(); j++) {
-                System.out.print(" -> "+adj.get(i).get(j).value);
+                System.out.print(" -> "+adj.get(i).get(j));
             }
             System.out.println();
         }
         System.out.println();
     }
 
-    static void BreadthFirstSearch(ArrayList<ArrayList<Vertex>> adj, Vertex s){
-        LinkedList<Vertex> queList = new LinkedList<>();
-        s.distance = 0;
-        s.color = "gray";
+    static void BreadthFirstSearch(ArrayList<ArrayList<Integer>> adj, int s, Boolean[] vertexVisit){
+        LinkedList<Integer> queList = new LinkedList<>();
         queList.offer(s);
+        vertexVisit[s] = true;
+        System.out.println(s);
         while(!queList.isEmpty()) {
-            Vertex u = queList.remove();
-            for (int i = 0; i < adj.get(u.value).size(); i++) {
-                Vertex v = adj.get(u.value).get(i);
-                if(v.color == "white") {
-                    v.color = "gray";
-                    v.distance = u.distance + 1;
-                    v.predessorVertex = u;
+            int u = queList.remove();
+            for (int i = 0; i < adj.get(u).size(); i++) {
+                int v = adj.get(u).get(i);
+                if(!vertexVisit[v]) {
                     queList.offer(v);
-                    System.out.println(v.value);
+                    vertexVisit[v] = true;
+                    System.out.println(v);
                 }
             }
-            u.color = "black";
         }
     }
     public static void main(String[] args) {
         int V = 5;
-        ArrayList<ArrayList<Vertex>> adj = new ArrayList<>(V);
-        ArrayList<Vertex> vertexArrayList = new ArrayList<>(V);
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>(V);
 
         for (int i = 0; i < V; i++) {
-            adj.add(new ArrayList<Vertex>());
+            adj.add(new ArrayList<Integer>());
         }
+        /*
+        0-----1---4
+        |     |  /
+        |     | /
+        |     |/
+        3-----2
+        */
+        addEdge(adj, 2, 3);
+        addEdge(adj, 0, 3);
+        addEdge(adj, 4, 1);
+        addEdge(adj, 0, 1);
+        addEdge(adj, 2, 1);
+        addEdge(adj, 2, 4);
 
-        // creating vertex and storing them in a vertexArrayList
-        for (int i = 0; i < V; i++) {
-            Vertex newVertex = new Vertex();
-            newVertex.value = i;
-            vertexArrayList.add(newVertex);
+        Boolean[] vertexVisit = new Boolean[V];
+        for (int i = 0; i < vertexVisit.length; i++) {
+            vertexVisit[i] = false;
         }
-        
-        addEdge(vertexArrayList, adj, 2, 3);
-        addEdge(vertexArrayList, adj, 0, 3);
-        addEdge(vertexArrayList, adj, 4, 1);
-        addEdge(vertexArrayList, adj, 0, 1);
-
         // printAdj(adj);
-        BreadthFirstSearch(adj, vertexArrayList.remove(0));
+        for (int i = 0; i < vertexVisit.length; i++) {
+            if(!vertexVisit[i]) {
+                BreadthFirstSearch(adj, i, vertexVisit);
+            }
+        }
     }
 }
